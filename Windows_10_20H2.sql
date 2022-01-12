8c61e951891e98f9072570806e5548a1b8b629be
Mike <michael.hartman0@gmail.com>
2022-01-12 17:59:55 -0500

    Create Windows_10_20H2.sql

---
+++ Windows_10_20H2.sql
@@ -0,0 +1,1 @@
+select *  from  SMS_R_System inner join SMS_G_System_OPERATING_SYSTEM on SMS_G_System_OPERATING_SYSTEM.ResourceId = SMS_R_System.ResourceId where SMS_G_System_OPERATING_SYSTEM.BuildNumber = "19042"
