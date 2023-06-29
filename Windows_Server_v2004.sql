b35dd7363ef39927b6ec73d9e7cf8f47f00808e5
Mike <michael.hartman0@gmail.com>
2023-06-29 10:51:11 -0700

    Delete Windows_Server_v2004.sql

--- Windows_Server_v2004.sql
+++
@@ -1,1 +0,0 @@
-select *  from  SMS_R_System inner join SMS_G_System_OPERATING_SYSTEM on SMS_G_System_OPERATING_SYSTEM.ResourceId = SMS_R_System.ResourceId where SMS_G_System_OPERATING_SYSTEM.BuildNumber = "19041"
